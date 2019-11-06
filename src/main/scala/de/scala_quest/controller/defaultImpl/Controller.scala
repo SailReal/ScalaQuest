387ecee948de9edeaca6a1f9d1bf65b081ca81db
Julian Raufelder <Julian@Raufelder.com>
2019-11-06 10:27:12 +0100

    Remove currently game from controller

--- src/main/scala/de/scala_quest/controller/defaultImpl/Controller.scala
+++ src/main/scala/de/scala_quest/controller/defaultImpl/Controller.scala
@@ -1,9 +1,8 @@
 package de.scala_quest.controller.defaultImpl
 
 import de.scala_quest.controller.{Controller => ControllerTrait}
-import de.scala_quest.model.Game
 
-class Controller(game: Game) extends ControllerTrait {
+class Controller() extends ControllerTrait {
 
   override def onQuit(): Unit = ???
 
