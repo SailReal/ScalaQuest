c3f1b06cf54f7bbb5772874a8a8b53983e132187
Julian Raufelder <Julian@Raufelder.com>
2019-11-06 21:19:17 +0100

    Inject game in controller

--- src/main/scala/de/scala_quest/GuiceModule.scala
+++ src/main/scala/de/scala_quest/GuiceModule.scala
@@ -2,11 +2,13 @@ package de.scala_quest
 
 import com.google.inject.AbstractModule
 import de.scala_quest.controller.Controller
+import de.scala_quest.model.Game
 
 
 class GuiceModule extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[Controller]).to(classOf[de.scala_quest.controller.defaultImpl.Controller])
+    bind(classOf[Game]).toInstance(de.scala_quest.model.defaultImpl.Game(List()))
   }
 }
--- src/main/scala/de/scala_quest/controller/defaultImpl/Controller.scala
+++ src/main/scala/de/scala_quest/controller/defaultImpl/Controller.scala
@@ -1,8 +1,10 @@
 package de.scala_quest.controller.defaultImpl
 
+import com.google.inject.Inject
 import de.scala_quest.controller.{Controller => ControllerTrait}
+import de.scala_quest.model.Game
 
-class Controller() extends ControllerTrait {
+class Controller @Inject()(game: Game) extends ControllerTrait {
 
   override def onQuit(): Unit = ???
 
